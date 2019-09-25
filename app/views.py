8bc72f8c287086b1b80e7e8978c9de38a9d066ce
jake <jake.labelle@hotmail.co.uk>
2019-09-25 23:14:27 +0100

    trying to get authentication

--- app/views.py
+++ app/views.py
@@ -4,15 +4,22 @@ import json, jsonify, collections, itertools, csv
 import requests
 import config as cfg
 from . import models
+from intuitlib.client import AuthClient
+from intuitlib.enums import Scopes
 
 
+access_token = ""
+refresh_token = ""
 
+@app.route("/redirectEnvironment")
+def redirect():
+    print("hi")
+    return "hi"
 
 
 @app.route("/")
 def index():
-    print(cfg.qbo)
-    return render_template('index.html')
+    return request.args.get('text', '')
 
 ################################################################################### SHOPIFY ORDER WEBHOOK
 
@@ -20,8 +27,8 @@ def index():
 def get_new_order():
 
     # this will have the order
-    payload = request.json
-    #payload = json.load(open('app/objects/shop_order_webhook.json'))
+    #payload = request.json
+    payload = json.load(open('app/objects/shop_order_webhook.json'))
 
     so = models.ShopOrder(payload['id'],payload['total_price'],payload['subtotal_price'],payload['financial_status'],payload['total_discounts'],
     payload['user_id'],payload['location_id'],payload['line_items'])
@@ -37,10 +44,29 @@ def get_new_order():
         ois.append(oi)
 
     print(sc.email)
-    quickbooks_cust_id = qbo_check_customer(sc)
 
+    requests.post(qbo_access_toke())
+    #quickbooks_cust_id = qbo_check_customer(sc)
+
+    return "abc"
 
 ################################################################################### CHECK QUICKBOOKS CUSTOMER
+
+
+def qbo_access_toke():
+    auth_client = AuthClient(
+    cfg.qbo['QBO_CLIENT_ID'],
+    cfg.qbo['QBO_CLIENT_SECRET'],
+    "https://tdasu.pagekite.me/redirect",
+    environment="production"
+    )
+
+    scopes = [
+        Scopes.ACCOUNTING,
+    ]
+    auth_url = auth_client.get_authorization_url(scopes)
+    print(auth_url)
+    return auth_url
 
 def qbo_check_customer(ShopCustomer):
     realm_id        = cfg.qbo['realmID']
@@ -48,9 +74,10 @@ def qbo_check_customer(ShopCustomer):
     query_statement = "SELECT * FROM Customer WHERE GivenName=%s AND FamilyName=%s" % (params1)
     params2         = (realm_id, query_statement)
     base_uri        = "https://quickbooks.api.intuit.com/v3/company/%s/query?query=%s" % (params2)
+    print(base_uri)
 
     qbo_query_id    = requests.get(base_uri)
-    qbo_query_id    = qbo_query.json()
+    print(qbo_query_id)
 
     if not qbo_query_id['QueryResponse']:
         return qbo_create_customer()
